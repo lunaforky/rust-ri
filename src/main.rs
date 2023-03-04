2e19d7ef7a0abbe44262a46fade4fea1a7b525fa
lunaforky <lunargaze03@gmail.com>
2023-03-04 10:49:00 +0800

    refactor: main fn

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,22 @@ mod parser;
 mod runner;
 mod utils;
 
-fn run() -> Result<(), CommonError> {
+fn main() -> Result<(), CommonError> {
     let opt = opt::Opt::from_args();
 
     let mut parser = parser::Parser::parser_opt(&opt)?;
 
     let cmd = parser.gene_command()?;
 
-    println!("Execute: {}", &cmd);
+    if cmd.len() > 0 {
+        println!("Execute: {}", &cmd);
+    }
 
-    if !opt.debug {
+    if opt.debug {
+        println!("Debug mode, not execute command");
+    } else {
         runner::Runner::run(&cmd)?;
     }
 
     Ok(())
-}
-
-fn main() -> () {
-    match run() {
-        Ok(_) => (),
-        Err(err) => eprintln!("{}", err),
-    }
 }
